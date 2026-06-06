1eb6433ba29691d8a27cac985b5b4083d7e68f5b
Yuvrajkalauni <yuvrajkalauni8@gmail.com>
2026-06-06 10:07:49 +0545

    working with models, creating table, ORM

--- django-june-1/product/models.py
+++ django-june-1/product/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=50, null=True)
+    address = models.CharField(max_length=80, null=True )
+    phone = models.IntegerField()
+  
+    class Meta:
+        db_table = 'Ram'
