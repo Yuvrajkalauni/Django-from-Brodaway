5365ef5cb7bd01b644e0aa841a76c3ec019dd42b
Yuvrajkalauni <yuvrajkalauni8@gmail.com>
2026-06-08 15:54:39 +0545

    day5 = models

--- self_django/product/models.py
+++ self_django/product/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Items(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.TextField()
+    phone_number = models.IntegerField(null=True)
+
+    class Meta:
+        db_table = "Things"
+
+    def __str__(self):
+        return f'{self.name}'
