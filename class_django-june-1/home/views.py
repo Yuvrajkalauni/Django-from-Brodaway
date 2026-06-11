e8d10622492fcb32f1e05134be136ecc06f3c590
Yuvrajkalauni <yuvrajkalauni8@gmail.com>
2026-06-11 23:29:06 +0545

    Removed unecessary files

--- class_django-june-1/home/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-def check(request):
-    return HttpResponse("Hello!, I king Raj kalauni.")
--- class_django-june-1/product/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def product_list(request):
-    data = Product.objects.all()
-    print(data)
-    return render(request, "itmes/things.html" )
